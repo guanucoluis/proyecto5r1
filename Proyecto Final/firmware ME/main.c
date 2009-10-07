adde3e6e424c28faf05562b3220324ec4efa7dbd
glvassia <glvassia@69626008-8548-11de-8681-f791fa4a598b>
2009-10-07 20:44:33 +0000

    Completé la configuración de las rutinas de servicio de interrupción (ISR) y agregué las ISR de la Base de tiempo normal y del A/D.

--- Proyecto Final/firmware ME/main.c
+++ Proyecto Final/firmware ME/main.c
@@ -79,24 +79,18 @@ COMENTARIO:
 											& T1_SYNC_EXT_OFF 
 											&	T1_SOURCE_INT	//Origen interno del clock
 											, PeriodoT1);
-					//EnableIntT1;	//Habilitar interrupción del Timer1
-					//SetPriorityIntT1(PrioridadT1);	//Setear nivel de prioridad del Timer1 a 7
 				//Configuración del Timer2 (Asignado a )
 					OpenTimer2(	T2_ON 
 											& T2_GATE_OFF 
 											& T2_PS_1_8 
 											&	T2_SOURCE_INT	//Origen interno del clock
 											, PeriodoT2);
-					//EnableIntT2;	//Habilitar interrupción del Timer2
-					//SetPriorityIntT2(PrioridadT2);	//Setear nivel de prioridad del Timer2
 				//Configuración del Timer2 (Asignado a )
 					OpenTimer3(	T3_ON 
 											& T3_GATE_OFF 
 											& T3_PS_1_8 
 											&	T3_SOURCE_INT	//Origen interno del clock
 											, PeriodoT3);
-					//EnableIntT3;	//Habilitar interrupción del Timer3
-					//SetPriorityIntT3(PrioridadT3);	//Setear nivel de prioridad del Timer3
 				//Configuración del A/D
 					//Configuración de ADCON1
         		ADCON1bits.FORM = 0b11;	//Formato del resultado: Fraccional con signo
@@ -131,39 +125,88 @@ COMENTARIO:
 					IPC2 = ((PrioridadAD * 0x1000));	//Asigno prioridad a AD
 					IPC4 = ((PrioridadINT1 * 0x0001));	//Asigno prioridad a INT1
 					IPC5 = ((PrioridadINT2 * 0x1000) | (PrioridadT5 * 0x0100) | (PrioridadT4 * 0x0010));	//Asigno prioridad a INT2, Timer5 y Timer4
-					
 				//Resetear Flags de Interrupción
 					IFS0 = 0;
 					IFS1 = 0;
 					IFS2 = 0;
-					
+				//Habilitar Interrupciones
+					IEC0 = 0b1000100010001001;
+					IEC1 = 0b0000000010000000;
+					IEC2 = 0b0000000000000000;
+				
 									
 Main:
-				//BLOQUE DE EJECUCIÓN DE PROCESOS
-					//Proceso/Rutina de Menu
-						if (Proc.HabRutMenu == 1)
-							if(Proc.EjecRutMenu == 1)
-							{
+			//BLOQUE DE EJECUCIÓN DE PROCESOS
+				//Proceso/Rutina de Menu
+					if (Proc.HabRutMenu == 1)
+						if(Proc.EjecRutMenu == 1)
+						{
 								
-								Proc.EjecRutMenu = 0;
-								goto Main;
-							}
-					//Proceso/Rutina de Teclado
-						if (Proc.HabRutTeclado == 1)
-							if(Proc.EjecRutTeclado == 1)
-							{
-								
-								Proc.EjecRutTeclado = 0;
-								goto Main;
-							}	
-					//Proceso/Rutina de Teclas
-						if (Proc.HabRutTeclas == 1)
-							if(Proc.EjecRutTeclas == 1)
-							{
-								
-								Proc.EjecRutTeclas = 0;
-								goto Main;
-							}
-	
+							Proc.EjecRutMenu = 0;
+							goto Main;
+						}
+				//Proceso/Rutina de Teclado
+					if (Proc.HabRutTeclado == 1)
+						if(Proc.EjecRutTeclado == 1)
+						{
+							
+							Proc.EjecRutTeclado = 0;
+							goto Main;
+						}	
+				//Proceso/Rutina de Teclas
+					if (Proc.HabRutTeclas == 1)
+						if(Proc.EjecRutTeclas == 1)
+						{
+							
+							Proc.EjecRutTeclas = 0;
+							goto Main;
+						}
+			goto Main;	
 			return 0;	
 		}  
+
+
+//RUTINAS DE SERVICIO DE INTERRUPCIÓN (ISR)
+	/*ISR del Timer1 (Base de Tiempo Normal)-----------------------------------------------------------------------------------------------------------------------
+	Descripción: Rutina de la Base de tiempo normal donde se decrementan los contadores de espera y se setean las banderas de ejecución
+	Entrada: nada
+	Salida: nada
+	//------------------------------------------------------------------------------------------------------------------------*/
+		void __attribute__((interrupt, auto_psv)) _T1Interrupt(void) //BASE DE TIEMPO NORMAL 
+		{
+			IFS0bits.T1IF = 0;
+			
+			//Proceso de Menú	
+				Proc.ContEspMenu--;
+				if (Proc.ContEspMenu == 0)
+				{
+					Proc.EjecRutMenu = 1;
+					Proc.ContEspMenu = CEMenu;
+				}
+			
+			//Proceso de Teclado	
+				Proc.ContEspTeclado--;
+				if (Proc.ContEspTeclado == 0)
+				{
+					Proc.EjecRutTeclado = 1;
+					Proc.ContEspTeclado = CETeclado;
+				}			
+
+			//Proceso de Teclas	
+				Proc.ContEspTeclas--;
+				if (Proc.ContEspTeclas == 0)
+				{
+					Proc.EjecRutTeclas = 1;
+					Proc.ContEspTeclas = CETeclas;
+				}	
+		}
+
+	/*ISR del AD (Base de Tiempo Normal)-----------------------------------------------------------------------------------------------------------------------
+	Descripción: Rutina de la Base de tiempo normal donde se decrementan los contadores de espera y se setean las banderas de ejecución
+	Entrada: nada
+	Salida: nada
+	//------------------------------------------------------------------------------------------------------------------------*/
+		void __attribute__((interrupt, auto_psv)) _ADCInterrupt(void)
+		{
+			IFS0bits.ADIF = 0;
+		}
