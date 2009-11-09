35cc344e82a353cc58d05b165cffb9a9a7d76ab7
glvassia <glvassia@69626008-8548-11de-8681-f791fa4a598b>
2009-11-09 22:11:56 +0000

    TP2 de Técnicas listo!!!

--- trunk/Catedras/Tecnicas Digitales III/TP2/Ej_2_1_6.m
+++ trunk/Catedras/Tecnicas Digitales III/TP2/Ej_2_1_6.m
@@ -1,23 +1,23 @@
 % Exponencial compleja
-
+ 
 figure(1)
-
+ 
 L=60;
-
+ 
 r=0.9;
 Xre = zeros(1,L);
 Xim = zeros(1,L);
-
+ 
 for n=0:L-1
     Xre(n+1)=(r^n)*cos((1/10)*n);
     Xim(n+1)=(r^n)*sin((1/10)*n);
 end
-
+ 
 n=0:L-1;
 subplot(2,1,1),
 stem(n,Xre),
 title('Parte Real');
-
+ 
 subplot(2,1,2),
 stem(n,Xim),
 title('Parte Imaginaria');
---
+++ trunk/Catedras/Tecnicas Digitales III/TP2/Ej_2_5_2.m
@@ -0,0 +1,31 @@
+% Convolucion-Deconvolucion
+ 
+x = [1 0 1 4 3 -1 1];
+ 
+h = [2 0 -3];
+ 
+n=-4:4;
+n1=-3:3;
+n2=-1:1;
+n3=-5:5;
+k=-20:20;
+ 
+%Convolucion de x(excitación) con h(kernel)
+y=conv(x,h); 
+ 
+%Deconvolucion de y(salida) con h(kernel) para obtener x(excitación)
+[x1,r] = deconv(y,h);
+ 
+figure(1);
+subplot(2,2,1);
+stem(n1,x);
+xlabel('x[n]');
+subplot(2,2,2);
+stem(n2,h);
+xlabel('h[n]');
+subplot(2,2,3);
+stem(n,y);
+xlabel('y = x[n] * h[n]');
+subplot(2,2,4);
+stem(n1,x1);
+xlabel('x1 = deconv(y,h)');
