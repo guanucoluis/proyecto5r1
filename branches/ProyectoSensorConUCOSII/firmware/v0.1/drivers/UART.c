3a31b7c3b2f1cc2639697f17596f9454d13e5156
guanucoluis <guanucoluis@69626008-8548-11de-8681-f791fa4a598b>
2014-02-25 04:36:55 +0000

    [BRANCH] Agrego archivos del módulo DIO y el proyecto para correr el UCOS-II en la PC. Elimino archivos del proyecto original que no son usados (NanoGUI_RTOS).

---
+++ branches/ProyectoSensorConUCOSII/firmware/v0.1/drivers/UART.c
@@ -0,0 +1,49 @@
+//#include <includes.h>
+#include <p33FJ128GP804.h>
+#include "UART.h"
+void InitUART1(void) {
+
+	// configure U1MODE
+	U1MODEbits.UARTEN = 0;	// Bit15 TX, RX DISABLED, ENABLE at end of func
+	//U1MODEbits.notimplemented;	// Bit14
+	U1MODEbits.USIDL = 0;	// Bit13 Continue in Idle
+	U1MODEbits.IREN = 0;	// Bit12 No IR translation
+	U1MODEbits.RTSMD = 0;	// Bit11 Simplex Mode
+	//U1MODEbits.notimplemented;	// Bit10
+	U1MODEbits.UEN = 0;		// Bits8,9 TX,RX enabled, CTS,RTS not
+	U1MODEbits.WAKE = 0;	// Bit7 No Wake up (since we don't sleep here)
+	U1MODEbits.LPBACK = 0;	// Bit6 No Loop Back
+	U1MODEbits.ABAUD = 0;	// Bit5 No Autobaud (would require sending '55')
+	U1MODEbits.URXINV = 0;	// Bit4 IdleState = 1  (for dsPIC)
+	U1MODEbits.BRGH = 0;	// Bit3 16 clocks per bit period
+	U1MODEbits.PDSEL = 0;	// Bits1,2 8bit, No Parity
+	U1MODEbits.STSEL = 0;	// Bit0 One Stop Bit
+	
+	//  U1BRG = (Fcy / (16 * BaudRate)) - 1
+	//  U1BRG = (39.804.000 / (16 * 9600)) - 1
+	//  U1BRG = 258.140625 //Round to 258
+	
+	U1BRG = 258;
+
+	// Load all values in for U1STA SFR
+	U1STAbits.UTXISEL1 = 0;	//Bit15 Int when Char is transferred (1/2 config!)
+	U1STAbits.UTXINV = 0;	//Bit14 N/A, IRDA config
+	U1STAbits.UTXISEL0 = 0;	//Bit13 Other half of Bit15
+	//U1STAbits.notimplemented = 0;	//Bit12
+	U1STAbits.UTXBRK = 0;	//Bit11 Disabled
+	U1STAbits.UTXEN = 0;	//Bit10 TX pins controlled by periph
+	U1STAbits.UTXBF = 0;	//Bit9 *Read Only Bit*
+	U1STAbits.TRMT = 0;	//Bit8 *Read Only bit*
+	U1STAbits.URXISEL = 0;	//Bits6,7 Int. on character recieved
+	U1STAbits.ADDEN = 0;	//Bit5 Address Detect Disabled
+	U1STAbits.RIDLE = 0;	//Bit4 *Read Only Bit*
+	U1STAbits.PERR = 0;		//Bit3 *Read Only Bit*
+	U1STAbits.FERR = 0;		//Bit2 *Read Only Bit*
+	U1STAbits.OERR = 0;		//Bit1 *Read Only Bit*
+	U1STAbits.URXDA = 0;	//Bit0 *Read Only Bit*
+
+	U1MODEbits.UARTEN = 1;	// And turn the peripheral on
+	U1STAbits.UTXEN = 1 ;
+	// I think I have the thing working now.
+}
+
